1f4efe098ae90709f45b7ddd495cd7d702db1fcc
Mohammed sayad <syadrca@gmail>
2023-02-20 14:37:48 -0800

    10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Description: base 10 with putchar
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
