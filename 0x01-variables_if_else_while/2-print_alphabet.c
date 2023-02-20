8543fe32ecbda5e0a0f38bd1fb61d46627928e43
Mohammed sayad <syadrca@gmail>
2023-02-20 11:08:12 -0800

    alphapet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+/**
+ * main - runs the program
+ * Description: loop on chars and concat them
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
