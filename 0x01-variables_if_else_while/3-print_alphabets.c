2dfefbfb70694398c9e1cd58ed369852fe491bd3
Mohammed sayad <syadrca@gmail>
2023-02-20 12:23:20 -0800

    alphBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
-
 /**
- * main - runs the program
- * Description: loop on chars and concat them
- * Return: 0
+* main - runs the program
+* Description: loop on chars and concat them
+* Return: 0
 */
-
 int main(void)
 {
-        int n = 97;
-	int i = 65;
-
-        while (n <= 122)
-        {
-                putchar(n);
-                n++;
-        }
-	while (i <= 90)
-	{
-		putchar(i);
-		i++;
-	}
-        putchar('\n');
-
-        return (0);
+int n = 97;
+int i = 65;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+while (i <= 90)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
 }
