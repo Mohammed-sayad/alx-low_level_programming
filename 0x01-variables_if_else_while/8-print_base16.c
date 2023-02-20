1753c8ce28a56bd2d2245fe44be957647c8c75e7
Mohammed sayad <syadrca@gmail>
2023-02-20 15:11:39 -0800

    FOR

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
+
 /**
-* main - runs the program
-* Description: loop on chars and concat them
-* Return: 0
+ * main - function
+ * Desciption: base 16 in lowercase
+ * Return: 0
 */
+
 int main(void)
 {
-int n;
-int c;
-for ( n = 48; n <= 56; n++)
-{
-putchar(n);
+	int c;
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+
+	for (c = 97; c <= 102; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
 }
-for ( c = 97; c <= 102; c++)
-{
-putchar(c);
-} 
-putchar('\n');   
-return (0);
-}
+
