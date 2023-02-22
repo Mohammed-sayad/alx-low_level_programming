39fb92b610725129548ea1e19a0b0701d2478052
Mohammed sayad <syadrca@gmail>
2023-02-22 13:15:32 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Function
+ * Description: return abs value of a given number
+ * @j: the giving value
+ * Return: value
+ */
+int _abs(int j)
+{
+	if (j == 0)
+	{
+		return (0);
+	}
+else
+	{
+		return (j * -1);
+	}
+}
